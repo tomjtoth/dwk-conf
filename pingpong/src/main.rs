a3739dbcc1a7b2ae14ee1f20002337f1121c41ac
Tamás Tóth <tomjtoth@gmail.com>
2025-12-23 10:44:04 +0200

    pingpong adjusted to 1.11 #deploy pingpong: 1.11

--- pingpong/src/main.rs
+++ pingpong/src/main.rs
@@ -1,17 +1,33 @@
-use std::env;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicU8, Ordering};
+use std::{
+    env,
+    fs::File,
+    io::{Seek, SeekFrom, Write},
+    sync::{
+        Arc, Mutex,
+        atomic::{AtomicU8, Ordering},
+    },
+};
 
 use axum::{Router, extract::State, routing::get};
 
 struct AppState {
     counter: AtomicU8,
+    file: Arc<Mutex<File>>,
 }
 
 #[tokio::main]
 async fn main() {
+    let pong_path = env::var("PONG_PATH").unwrap_or(String::from("data/pong"));
+
     let state = Arc::new(AppState {
         counter: AtomicU8::new(0),
+        file: Arc::new(Mutex::new(
+            File::options()
+                .write(true)
+                .create(true)
+                .open(&pong_path)
+                .expect(&format!(r#"unable to open PONG_PATH="{}""#, pong_path)),
+        )),
     });
 
     let app = Router::new()
@@ -19,6 +35,13 @@ async fn main() {
             "/pingpong",
             get(|State(state): State<Arc<AppState>>| async move {
                 let current = state.counter.fetch_add(1, Ordering::SeqCst);
+
+                if let Ok(mut file) = state.file.lock() {
+                    let _ = file.set_len(0);
+                    let _ = file.seek(SeekFrom::Start(0));
+                    let _ = write!(file, "{}", current);
+                }
+
                 format!("pong {}", current)
             }),
         )
@@ -30,6 +53,6 @@ async fn main() {
         .await
         .unwrap();
 
-    println!("Server started at port {}", &port);
+    println!("listening at :{}/pingpong", &port);
     axum::serve(listener, app).await.unwrap();
 }
